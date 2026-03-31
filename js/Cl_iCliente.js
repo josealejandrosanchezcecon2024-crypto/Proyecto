e59cf1a1283e01e3fd2e6906e96cf3f52964e764
Carlos D <1001.31423494.ucla@gmail.com>
2026-03-31 19:34:01 -0400

    Add Cl_iCliente class for client data handling

---
+++ js/Cl_iCliente.js
@@ -0,0 +1,17 @@
+export default class Cl_iCliente {
+    leernombre() {
+        return prompt("Introduzca su nombre");
+    }
+    leertipoVehiculo() {
+        return prompt("Introduzca su tipo de vehículo: Moto, Paseo, Camioneta");
+    }
+    leercantidadKilometros() {
+        return prompt("Introduzca la cantidad de kilómetros recorridos (numéricamente), mg/lc");
+    }
+    reporteCliente(n, t, c, mT) {
+        return `Nombre: ${n}
+                <br>Tipo de vehículo: ${t}
+                <br>Kilómetros: ${c}
+                <br>Monto total a pagar: ${mT}`;
+    }
+}
