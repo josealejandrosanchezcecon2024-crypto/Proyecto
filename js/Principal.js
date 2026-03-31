35de4ca91957564cc94d225774326f742cfaca85
Carlos D <1001.31423494.ucla@gmail.com>
2026-03-31 19:34:56 -0400

    Add Principal.js for TaxiTour client management

---
+++ js/Principal.js
@@ -0,0 +1,14 @@
+// La empresa de Transporte “TaxiTour”, ofrece viajes en diferentes tipos de Vehículos: Moto, De Paseo, Camioneta. De cada cliente se conoce: Nombre, tipo de vehículo y cantidad de kms; además se sabe que el cliente paga una tarifa mínima por viaje hasta 4.5 kms.
+
+import Cl_Cliente from "./Cl_Cliente";
+import Cl_iCliente from "./Cl_iCliente";
+
+let icliente = new Cl_iCliente (),
+n = icliente.leernombre(),
+t = icliente.leertipoVehiculo(),
+c = icliente.leercantidadKilometros(),
+cliente = new Cl_Cliente(n, t, c);
+
+let salida = document.getElementById ("salida");
+salida.innerHTML =
+ icliente.reporteCliente(cliente.nombre, cliente.tipoVehiculo, cliente.cantidadKilometros, cliente.montoTotal());
