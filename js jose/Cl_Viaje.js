e3e88c0e4d3863478c4c7760227251cefd97bbf9
Jose Sánchez <josealejandrosanchezcecon2024@gmail.com>
2026-04-01 17:35:02 -0400

    Add files via upload

--- js jose/Cl_Viaje.js
+++ js jose/Cl_Viaje.js
@@ -4,7 +4,7 @@ export default class Cl_Viaje {
     constructor(nombre,tipoVehiculo,cantidadKm) {
         this.nombre = nombre ;
         this.tipoVehiculo = tipoVehiculo ;
-        this.cantidadKm = cantidadKm ;
+        this.cantidadKm = +cantidadKm ;
     }
 set nombre (n) {
     this._nombre = n ;
@@ -25,26 +25,19 @@ get cantidadKm () {
     return this._cantidadKm ;
 }
 montoPagar () {
-     let tarifa;
     switch (this._tipoVehiculo) {
         case "1":
-            tarifa = 1.50;
-            break;
+            return 1.50;
         case "2":
-            tarifa = 4.00;
-            break;
+            return 4.00;
         case "3":
-            tarifa = 5.00;
-            break;
-        default:
-            tarifa = 0;
-    }
-    return this.cantidadKm * tarifa;
-}
+            return 5.00;
+            default:
+                return 0;
     }
 }
 montoAdicional () {
-    if (this._tipoVehiculo === "1" && this.cantidadKm > 4.50)
+   if (this._tipoVehiculo === "1" && this.cantidadKm > 4.50)
         return this.montoPagar() * 0.10 ;
     else if (this._tipoVehiculo === "2" && this.cantidadKm > 4.50)
         return this.montoPagar() * 0.05 ;
