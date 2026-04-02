470ffc45b9700d3cad9c7a73a475b61e7140052e
Carlos D <1001.31423494.ucla@gmail.com>
2026-04-01 22:15:39 -0400

    Update import statements and reportClient call

--- js/Principal.js
+++ js/Principal.js
@@ -1,7 +1,7 @@
 // La empresa de Transporte “TaxiTour”, ofrece viajes en diferentes tipos de Vehículos: Moto, De Paseo, Camioneta. De cada cliente se conoce: Nombre, tipo de vehículo y cantidad de kms; además se sabe que el cliente paga una tarifa mínima por viaje hasta 4.5 kms.
 
-import Cl_Cliente from "./Cl_Cliente";
-import Cl_iCliente from "./Cl_iCliente";
+import Cl_Cliente from "./Cl_Cliente.js";
+import Cl_iCliente from "./Cl_iCliente.js";
 
 let icliente = new Cl_iCliente (),
 n = icliente.leernombre(),
@@ -11,4 +11,11 @@ cliente = new Cl_Cliente(n, t, c);
 
 let salida = document.getElementById ("salida");
 salida.innerHTML =
- icliente.reporteCliente(cliente.nombre, cliente.tipoVehiculo, cliente.cantidadKilometros, cliente.montoTotal());
+ icliente.reporteCliente(
+    cliente.nombre, 
+    cliente.tipoVehiculo, 
+    cliente.cantidadKilometros, 
+    cliente.kmsExtra,           
+    cliente.montoAdicional(),   
+    cliente.montoTotal()
+ );
